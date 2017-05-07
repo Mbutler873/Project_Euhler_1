279303703e40035f9a47f2c20781c7e634154baf
Mbutler873 <mab363@zips.uakron.edu>
2017-05-07 14:35:01 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@ This program finds the sum of all the multiples of 3 and 5 between 1 and 1000
 
 int main()
 {
+    //an array to store all the multiples of 3 and 5
     double addins[1000] = {3, 5};
     int index = 2;
     int total = 0;
 
+    //a loop that runs loops through each integer to check if it
+    //is a multiple of 3 or 5
     for(int i = 6; i < 1000; i++)
     {
+        //checks integers and adds them to the array
         if(i % 3 == 0 || i % 5 == 0)
         {
             addins[index] = i;
@@ -20,6 +24,7 @@ int main()
         }
     }
 
+    //loops through the array and adds together all of the elements
     for(int i = 0; i < 1000; i++)
     {
         total += addins[i];
